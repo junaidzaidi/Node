2ab502d03c9b4dc9b1a5e05c13c688b9a8373f1f
Junaid Zaidi <51705693+junaidzaidi@users.noreply.github.com>
2023-07-19 19:43:04 -0700

    Basic CRUD implementation

--- Learning Path/Express Framework/index.js
+++ Learning Path/Express Framework/index.js
@@ -1,5 +1,12 @@
-const express = require('express');
 
+/*
+    This file is an introduction to express and how to make basic CRUD operations.
+*/
+
+// Input validation package
+const Joi = require('joi')
+
+const express = require('express');
 /*
     express() create an express application
 
@@ -10,6 +17,9 @@ const express = require('express');
         app.delete()
 */
 const app = express();
+
+// Add and use JSON as a middleware in the request processing pipeline in order to read JSON from body properly.
+app.use(express.json())
 
 // Getting the environment variable for which PORT to listen on.
 const port = process.env.PORT || 3000;
@@ -57,19 +67,76 @@ app.get('/api/courses', (req, res) => {
 // Have id as a route parameter.
 app.get('/api/courses/:id', (req, res) => {
     const requestedId = req.params.id
-    const course = courses.find(obj => obj.id == requestedId) || "No Record Found"
-    // res.send(req.params)
+    const course = courses.find(obj => obj.id == requestedId)
+    
+    if (!course) return res.status(404).send('The course with the given Id was not found')
+    res.send(course)
+    // res.end()
+});
+
+// Route parameters are nothing just a Dictionary of URL parameters.
+// Express also handles query string parameters like ...?sortBy="id"&offset=20&start=50 etc
+
+// Route parameters are used for required values.
+// Query string parameters are used for optional values.
+app.get('/api/courses/:id/:name', (req, res) => {
+    requestParams = {
+        routeParameters: req.params,
+        queryParameters: req.query
+    }
+
+    res.send(requestParams)
+    res.end()
+});
+
+// Post Request
+// For adding new course
+// Generally ID is assigned by the database. For testing purpose, just assigning it manually.
+app.post('/api/courses', (req, res) => {
+
+    // Input Validations
+    if (!req.body.name) return res.status(400).send('Name is required and should be minimum 3 characters')
+    if (req.body.name.length < 3) return res.status(400).send('Name is required and should be minimum 3 characters')
+    
+    // Input Validations can be simplified by JOI package
+    // For that first we define a schema
+    // And validate like this
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+
+    result = Joi.validate(req.body, schema)
+    if (result.error) return res.status(400).send(result.error.details[0].message)
+    
+    const course = {
+        id: `ENPM ${Math.floor((Math.random() * 1000))}`,
+        courseName: req.body.name
+    }
+    courses.push(course)
     res.send(course)
     res.end()
 });
 
-// Route parameters are nothing just a Dictionary of URL parameters.
-app.get('/api/courses/:id/:name', (req, res) => {
-    const requestedId = req.params.id
-    res.send(req.params)
-    res.end()
+// For updating any course we use PUT methods
+app.put('/api/courses/:id', (req, res) => {
+    // Look for course
+    // If not exist send 404
+
+    // Look for body
+    // If not valid send 400 - Bad Reques
+
+    // Update Course
+    // Return Updated Course Object
 });
 
+// For deleting any course we use DELETE methods
+app.delete('/api/courses/:id', (req, res) => {
+    // Look for index of course
+    // If not exist send 404
+
+    // Delete Course
+    // Return Deleted Course Object
+})
 
 /*
     Specifies the port we want to listen for HTTP calls
